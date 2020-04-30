ee4e4542f00fec39fd783e9f755bfe0767cd00f6
vinodkumar898 <vinod.kumar@shrisinfotech.com>
2020-04-30 16:13:51 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { NotesListComponent } from './notes-list/notes-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotesListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
